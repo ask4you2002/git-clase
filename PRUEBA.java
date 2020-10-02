9768b3e072352c067dc20caa46bd8ade22b10b1a
jarabe <ask4you2002@hotmail.com>
2020-10-02 14:12:05 +0200

    Nombre completo, Fecha denacimiento,Lugar de residencia y version de so

--- PRUEBA.java
+++ PRUEBA.java
@@ -2,6 +2,7 @@ public class PRUEBA {
     public static void main(String args[]) {
         System.out.println("Jaime Alvarez Lopez");
 	System.out.println("León, 16/05/2002");
+	System.out.println("Trobajo del Camino, Linux MINT");
     }
 
 }
