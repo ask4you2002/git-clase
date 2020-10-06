4c5153a025e28755eaa230a18358674d38130a8e
jarabe <ask4you2002@hotmail.com>
2020-10-06 09:27:11 +0200

    PRUEBA.java

--- PRUEBA.java
+++ PRUEBA.java
@@ -2,8 +2,9 @@ public class PRUEBA
 {
     public static void main(String args[]) {
         System.out.println("Jaime Alvarez Lopez");
-	    System.out.println("León, 16/05/2002");
-	    System.out.println("Trobajo del Camino, Linux MINT");
+	System.out.println("León, 16/05/2002");
+	System.out.println("Trobajo del Camino, Linux MINT");
+	System.out.println("Jaime Alvarez Lopez");
     }
 
 }
